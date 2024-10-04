160c3a38492c5f1bc0a6621a96a0d69d62e77dd4
RafaelSvgh <salvatierravallerafael@gmail.com>
2024-10-04 07:10:44 -0400

    http to https

--- resources/views/livewire/diagrama/diagram-colaboradores.blade.php
+++ resources/views/livewire/diagrama/diagram-colaboradores.blade.php
@@ -7,7 +7,7 @@
                 class="w-2/4 form-input pl-9  focus:ring-indigo-500 focus:border-indigo-500" />
             <span class="absolute pr-3 pt-2.5 inset-0 right-auto  ">
                 <svg class=" w-4 h-4 shrink-0 fill-current text-slate-400 group-hover:text-slate-500 ml-3 mr-2"
-                    viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+                    viewBox="0 0 16 16" xmlns="https://www.w3.org/2000/svg">
                     <path
                         d="M7 14c-3.86 0-7-3.14-7-7s3.14-7 7-7 7 3.14 7 7-3.14 7-7 7zM7 2C4.243 2 2 4.243 2 7s2.243 5 5 5 5-2.243 5-5-2.243-5-5-5z" />
                     <path
